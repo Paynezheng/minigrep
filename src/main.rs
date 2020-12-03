74adfc78ca1284d51de8b21167130c3e42fbef28
Payne <3334248237@qq.com>
2020-12-03 11:41:44 +0800

    重构改进模块性和错误处理

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use std::env;
+use std::process;
+
+use minigrep::Config;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let query = &args[1];
-    let filename = &args[2];
-    println!("This is query: {}!😈",query);
-    println!("This is filename: {}!👾",filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {}",err);
+            process::exit(1);
+            });
+
+    println!("This is query: {}!😈🚀",config.query);
+    println!("This is filename: {}!👾",config.filename);
+
+    if let Err(e) =minigrep::run(config) {
+        println!("Application error: {}",e);
+        
+        process::exit(1);
+    }
 }
