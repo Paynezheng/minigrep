65ac07a98b89f551cd1176aeaf942fd8d67e638b
Payne <3334248237@qq.com>
2020-12-05 15:27:26 +0800

    获取环境变量并将错误输出到标准错误

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 use std::error::Error;
 use std::fs;
+//读取环境变量
+use std::env;
+
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -14,14 +18,24 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        //env::var检查环境变量
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
 pub fn run(config: Config) -> Result<(),Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    for line in search(&config.query,&contents) {
-        println!("{}",line);
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
     }
 
     Ok(())
@@ -36,6 +50,22 @@ pub fn search<'a>(_query: &str,_contents: &'a str) -> Vec<&'a str> {
     }
     results
 }
+
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+
 
 #[cfg(test)]
 mod tests {
@@ -54,6 +84,21 @@ safe,fast,productive.
             vec!["safe,fast,productive."],
             search(query,contents)
             );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUST";
+        let contents = "\
+Rust是最好的语言！
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust是最好的语言！", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
 
