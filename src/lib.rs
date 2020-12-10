9e81189f068037246fd1c196856c61a75d78c666
Payne <3334248237@qq.com>
2020-12-10 20:23:34 +0800

    使用迭代器和闭包改进项目的性能

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,26 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+
+    //因为迭代器返回了数据的所有权，就无需复制内存和数据
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        //env::var检查环境变量
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
+        /*
         if args.len() < 3 {
             return Err("Not enough arguments");
         }
@@ -22,6 +41,7 @@ impl Config {
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config { query, filename, case_sensitive })
+        */
     }
 }
 
@@ -42,6 +62,11 @@ pub fn run(config: Config) -> Result<(),Box<dyn Error>> {
 }
 
 pub fn search<'a>(_query: &str,_contents: &'a str) -> Vec<&'a str> {
+    // 迭代器 + 闭包更适合用来做过滤数据
+    _contents.lines()
+    .filter(|line| line.contains(_query))
+    .collect()
+    /*
     let mut results = Vec::new();
     for line in _contents.lines() {
         if line.contains(_query){
@@ -49,20 +74,24 @@ pub fn search<'a>(_query: &str,_contents: &'a str) -> Vec<&'a str> {
         }
     }
     results
+    */
 }
 
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines()
+    .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+    .collect()
+    /*
     let query = query.to_lowercase();
     let mut results = Vec::new();
-
     for line in contents.lines() {
         if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
-
     results
+    */
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    //let args: Vec<String> = env::args().collect();
+    //将迭代器作为参数
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
             eprintln!("Problem parsing arguments: {}",err);
             process::exit(1);
             });
